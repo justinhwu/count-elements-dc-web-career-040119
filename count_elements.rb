20810f4d0a400721afc59ffd23a4b7a66b2da4f0
Justin Wu <justinhwu95@gmail.com>
2019-04-02 00:25:34 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -3,7 +3,7 @@ def count_elements(array)
   new_hash = {}
   counter = 0 
   array.each do |names|
-    counter = count(names)
+    counter = array.count(names)
     new_hash[names] = counter
   end
     new_hash
